8cd9e5cedd89724c3ab16d8b4deb7671d9785fdd
Narender <narendernukala@gmail.com>
2018-08-26 10:10:47 +0530

    Initial Commit

---
+++ SeleniumDemo/src/basicprgs/SeleniumBasic.java
@@ -0,0 +1,13 @@
+package basicprgs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumBasic {
+	
+	public static void main(String args[]) {
+		
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
